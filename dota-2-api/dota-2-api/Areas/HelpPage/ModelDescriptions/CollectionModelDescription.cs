375957a203570c664cdf314128a948e2ea3f837f
Stephen Christopher Adams <vengeanz@gmail.com>
2016-05-07 18:53:14 +1200

    Merge remote-tracking branch 'refs/remotes/origin/First-Branch'
    asdf

---
+++ dota-2-api/dota-2-api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace dota_2_api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ dota-2-api/dota-2-api/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace dota_2_api.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
