2b3c6df56ec4cb7436c10ced759ad428e46c6de2
Stephen Christopher Adams <vengeanz@gmail.com>
2016-05-07 18:34:49 +1200

    Initial project commit
    Initial project commit

---
+++ dota-2-api/dota-2-api/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace dota_2_api.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
